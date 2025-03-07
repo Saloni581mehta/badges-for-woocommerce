511fdba45d53287625b8398a5de44378b429a3c1
saloni-mehta210 <saloni.alpode@gmail.com>
2025-03-07 10:55:54 +0545

    Added plugin header requirements

---
+++ badges-for-woocommerce.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('ABSPATH') or die('No script kiddies please!!');
+/*
+ * Plugin Name: Badges For WooCommerce
+ * Plugin URI: https://example.com/wordpress-plugins/badges-woocommerce/
+ * Description: A plugin to show badges in your WooCommerce store.
+ * Version: 	1.0.0
+ * Author:     Saloni
+ * Author URI:  https://example.com/
+ * Domain Path: /languages
+ * Text Domain: badges-for-woocommerce
+ * Requires at least: 6.6 or higher
+ * Requires PHP:      7.2 or higher
+ * WC requires at least: 5.0
+ * WC tested up to: 9.7.1
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
