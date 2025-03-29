918dce7ffd305077ce4d337d0a2b9acfc5b152b2
Saloni Mehta <saloni581mehta@gmail.com>
2025-03-29 14:40:27 +0545

    removed unwanted code

--- badges-for-woocommerce.php
+++ badges-for-woocommerce.php
@@ -161,8 +161,6 @@ function bgfw_display_discount_badge()
     global $product;
 
     if ($product->is_on_sale() && $product->is_in_stock()) {
-        // $off_text = __('OFF', 'badges-for-woocommerce');
-        // echo '<div class="bgfw-sales-badges">-' . bgfw_sale_price_items($product) . ' ' . $off_text . '</div>';
         // Check if the product was published within the last 7 days
         $publish_date = $product->get_date_created();
         $current_date = current_time('timestamp');
